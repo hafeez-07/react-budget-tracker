afc465b1daa6caec12adac55d37e4064f7f64a49
hafeez-07 <hafeezmohammad689@gmail.com>
2026-03-03 06:32:29 +0530

    remove unused import of useRef in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback, useRef } from "react";
+import { useState, useEffect, useCallback } from "react";
 import Navbar from "./components/Navbar";
 import TransactionForm from "./components/TransactionForm";
 import TransactionList from "./components/TransactionList";
